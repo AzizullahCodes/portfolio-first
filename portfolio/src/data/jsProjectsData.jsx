7fbdada893e8a2263b08c97c1758a54035250eac
Azizullah <azizullahcodes@gmail.com>
2026-02-14 23:32:12 +0500

    jsProjectData created

---
+++ portfolio/src/data/jsProjectsData.jsx
@@ -0,0 +1,136 @@
+// jsProjectsData.jsx 
+// cssProjectsData.jsx
+
+import React from "react";
+import letterCounter from '../assets/images/javascriptImages/letterCounter.PNG'
+import bulbOnOff from '../assets/images/javascriptImages/bulbOnOff.PNG';
+import swappingImages from '../assets/images/javascriptImages/swappingImages.PNG';
+import hashtagGenerator from '../assets/images/javascriptImages/hashtagGenerator.PNG';
+import palindromeChecker from '../assets/images/javascriptImages/palindromeChecker.PNG';
+import autoBackgroundColorChanger from '../assets/images/javascriptImages/autoBackgroundColorChanger.PNG';
+import autoImageSlider from '../assets/images/javascriptImages/autoImageSlider.PNG';
+import cnicMasker from '../assets/images/javascriptImages/cnicMasker.PNG';
+import simpleImageSlider from '../assets/images/javascriptImages/simpleImageSlider.PNG';
+import multiplestylechanger from '../assets/images/javascriptImages/multiplestylechanger.PNG';
+import vowelConsonantSeperator from '../assets/images/javascriptImages/vowelConsonantSeperator.PNG';
+import miniBankingSystem from '../assets/images/javascriptImages/miniBankingSystem.PNG';
+import urlGenerator from '../assets/images/javascriptImages/urlGenerator.PNG';
+import jsSignup from '../assets/images/javascriptImages/jsSignup.PNG';
+
+
+const jsProjectsData = [
+    {
+        id : 1,
+        title :"Project-01 screenshot, live & github link" ,
+        image :letterCounter ,
+        live :"https://javascript-tasks-mauve.vercel.app/",
+        github :"https://github.com/AzizullahCodes/javascriptTasks/tree/main/letterCounter",
+        
+    },
+    {
+        id : 2,
+        title :"Project-02 screenshot, live & github link" ,
+        image :bulbOnOff ,
+        live :"https://javascript-tasks-3lxt.vercel.app/",
+        github :"https://github.com/AzizullahCodes/javascriptTasks/tree/main/bulbOnOff",
+        
+    },
+    {
+        id : 3,
+        title :"Project-03 screenshot, live & github link" ,
+        image :swappingImages ,
+        live :"https://javascript-tasks-341l.vercel.app/",
+        github :"https://github.com/AzizullahCodes/javascriptTasks/tree/main/swappingImages",
+        
+    },
+    {
+        id : 4,
+        title :"Project-04 screenshot, live & github link" ,
+        image :hashtagGenerator ,
+        live :"https://javascript-tasks-65a8.vercel.app/" ,
+        github :"https://github.com/AzizullahCodes/javascriptTasks/tree/main/hashtageGenerator",
+        
+    },
+    {
+        id : 5,
+        title :"Project-05 screenshot, live & github link" ,
+        image :palindromeChecker ,
+        live :"https://javascript-tasks-sg4s.vercel.app/",
+        github :"https://github.com/AzizullahCodes/javascriptTasks/tree/main/palindromeChecker",
+        
+    },
+    {
+        id : 6,
+        title :"Project-06 screenshot, live & github link" ,
+        image :autoBackgroundColorChanger ,
+        live :"https://javascript-tasks-da1h.vercel.app/",
+        github :"https://github.com/AzizullahCodes/javascriptTasks/tree/main/autoBackgroundColorChanger",
+        
+    },
+    {
+        id : 7,
+        title :"Project-07 screenshot, live & github link" ,
+        image :autoImageSlider ,
+        live :"https://javascript-tasks-y8ft.vercel.app/",
+        github :"https://github.com/AzizullahCodes/javascriptTasks/tree/main/autoSlider",
+        
+    },
+    {
+        id : 8,
+        title :"Project-08 screenshot, live & github link" ,
+        image :simpleImageSlider ,
+        live :"https://javascript-tasks-s9ng.vercel.app/",
+        github :"https://github.com/AzizullahCodes/javascriptTasks/tree/main/simpleImageSlider",
+        
+    },
+    {
+        id : 9,
+        title :"Project-09 screenshot, live & github link" ,
+        image :multiplestylechanger ,
+        live :"https://javascript-tasks-pc8l.vercel.app/" ,
+        github :"https://github.com/AzizullahCodes/javascriptTasks/tree/main/multipleStyleChanger" ,
+        
+    },
+    {
+        id : 10,
+        title :"Project-10 screenshot, live & github link" ,
+        image :vowelConsonantSeperator ,
+        live :"https://javascript-task-two.vercel.app/",
+        github :"https://github.com/AzizullahCodes/javascriptTaskTwo/tree/main/vowelConsonatExtractor",
+        
+    },
+    {
+        id : 11,
+        title :"Project-11 screenshot, live & github link" ,
+        image :miniBankingSystem ,
+        live :"https://javascript-task-two-i58z.vercel.app/",
+        github :"https://github.com/AzizullahCodes/javascriptTaskTwo/tree/main/miniBankingSystem",
+        
+    },
+    {
+        id : 12,
+        title :"Project-12 screenshot, live & github link" ,
+        image :urlGenerator ,
+        live :"https://javascript-task-two-qbmn.vercel.app/",
+        github :"https://github.com/AzizullahCodes/javascriptTaskTwo/tree/main/urlGenerator",
+        
+    },
+    {
+        id : 13,
+        title :"Project-13 screenshot, live & github link" ,
+        image :jsSignup ,
+        live :"https://javascript-task-two-6efe.vercel.app/" ,
+        github :"https://github.com/AzizullahCodes/javascriptTaskTwo/tree/main/signupLogin",
+        
+    },
+    {
+        id : 14,
+        title :"Project-14 screenshot, live & github link" ,
+        image :cnicMasker ,
+        live :"https://javascript-tasks-voee.vercel.app/",
+        github :"https://github.com/AzizullahCodes/javascriptTasks/tree/main/nicMasker",
+        
+    },
+]
+
+export default jsProjectsData;
