e8da93084bc4bacf4133c7f8d5a9b82aadc8672c
Azizullah <azizullahcodes@gmail.com>
2026-02-15 11:52:22 +0500

    Navlink added

--- portfolio/src/components/navbar/navbar.jsx
+++ portfolio/src/components/navbar/navbar.jsx
@@ -17,13 +17,20 @@ function MyNavbar() {
 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+            {/* <Nav.Link as={NavLink} to="/">Home</Nav.Link>
             <Nav.Link as={NavLink} to="/about">About</Nav.Link>
             <Nav.Link as={NavLink} to="/services">Services</Nav.Link>
             <Nav.Link as={NavLink} to="/cssProjects">Css-Projects</Nav.Link>
             <Nav.Link as={NavLink} to="/jsProjects">JsProjects</Nav.Link>
             <Nav.Link as={NavLink} to="/codingExercises">Coding-Exercises</Nav.Link>
-            <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
+            <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link> */}
+            <NavLink className="nav-link" to="/">Home</NavLink>
+          <NavLink className="nav-link" to="/about">About</NavLink>
+          <NavLink className="nav-link" to="/services">Services</NavLink>
+          <NavLink className="nav-link" to="/cssProjects">Css-Projects</NavLink>
+          <NavLink className="nav-link" to="/jsProjects">JsProjects</NavLink>
+          <NavLink className="nav-link" to="/codingExercises">Coding-Practice</NavLink>
+          <NavLink className="nav-link" to="/contact">Contact</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
